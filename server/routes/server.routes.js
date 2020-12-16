35eef091dfdf2f34f5597c302ff90aea1a13082a
Matt Sontag <mrsontag@gmail.com>
2020-12-16 10:59:47 -0500

    Updated server files to avoid .env conflict

--- server/routes/server.routes.js
+++ server/routes/server.routes.js
@@ -5,7 +5,7 @@ const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa');
 require('dotenv').config();
 
-if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+if (!process.env.AUTH0_DOMAIN_PHOTO || !process.env.AUTH0_AUDIENCE_PHOTO) {
     throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
 }
 
@@ -15,12 +15,12 @@ const checkJwt = jwt({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+        jwksUri: `https://${process.env.AUTH0_DOMAIN_PHOTO}/.well-known/jwks.json`
     }),
 
     // Validate the audience and the issuer.
-    audience: process.env.AUTH0_AUDIENCE,
-    issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+    audience: process.env.AUTH0_AUDIENCE_PHOTO,
+    issuer: `https://${process.env.AUTH0_DOMAIN_PHOTO}/`,
     algorithms: ['RS256']
 });
 const checkScopes = jwtAuthz(['read:messages']);
